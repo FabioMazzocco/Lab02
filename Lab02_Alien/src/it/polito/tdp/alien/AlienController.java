349d3857fa91b78065e2a0ebb663dceb0299dc26
fabin <fabin@169.254.79.176>
2019-03-13 14:38:04 +0100

    Completed & modified for excercise 2 with substitution of String translation w/ LinkedList<String> translation

--- Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -1,12 +1,13 @@
 package it.polito.tdp.alien;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import it.polito.tdp.alien.model.*;
 
 public class AlienController {
@@ -14,8 +15,7 @@ public class AlienController {
 	AlienModel model;
 	
 	public AlienController() {
-		model=null;
-		txtArea.appendText("Welcome to Alien Translator v2019");
+		model = null;
 	}
 	
     @FXML
@@ -31,46 +31,51 @@ public class AlienController {
     private Button btnTranslate;
 
     @FXML
-    private TextField txtNewWord;
-
-    @FXML
     private TextArea txtArea;
-    
-    @FXML
-    private Button btnAddWord;
 
     @FXML
-    void doAddWord(ActionEvent event) throws Exception {
+    private Button btnClear;
+
+    @FXML
+    void doClear(ActionEvent event) {
     	txtArea.clear();
-    	String word = txtNewWord.getText().trim();
-    	if(!word.contains(" ")) {
-    		txtArea.appendText("Formato errato");
-    		return;
-    	}
-    	String translation = word.substring(word.indexOf(" ")+1).trim();
-    	word = word.substring(0, word.indexOf(" ")).trim();
-    	try{
-    		model.addWord(word, translation);
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    	txtArea.appendText("Aggiunta '"+word+"' con traduzione '"+translation+"'\n");
-    	txtNewWord.clear();
-    }
-
-    @FXML
-    void doTranslate(ActionEvent event) {
-    	String word = txtTranslation.getText().trim();
-    	txtTranslation.clear();
     }
 
     @FXML
     void initialize() {
         assert txtTranslation != null : "fx:id=\"txtTranslation\" was not injected: check your FXML file 'Alien.fxml'.";
         assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
-        assert txtNewWord != null : "fx:id=\"txtNewWord\" was not injected: check your FXML file 'Alien.fxml'.";
-        assert btnAddWord != null : "fx:id=\"btnAddWord\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'Alien.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Alien.fxml'.";
 
+    }
+
+    @FXML
+    void doTranslate(ActionEvent event) throws Exception {
+    	String word = txtTranslation.getText().trim();
+    	LinkedList<String> translations = new LinkedList<String>();
+    	if(!word.contains(" ")) {
+    		//Qui per tradurre una parola
+    		try{
+    			translations = model.findWord(word);
+    		}catch(Exception e) {
+    			e.printStackTrace();
+    		}
+    		txtArea.appendText(toString(translations));
+    	}
+    	else {
+    		//Qui per aggiungere una parola
+    		txtArea.appendText("\n");
+    		String translation = word.substring(word.indexOf(" ")+1).trim();
+	    	word = word.substring(0, word.indexOf(" ")).trim();
+	    	try{
+	    		model.addWord(word, translation);
+	    	}catch(Exception e) {
+	    		e.printStackTrace();
+	    	}
+	    	txtArea.appendText("Aggiunta '"+word.toLowerCase()+"' con traduzione '"+translation.toLowerCase()+"'\n");
+    	}
+    	txtTranslation.clear();
     }
     
     public void setModel(AlienModel model) {
@@ -79,5 +84,12 @@ public class AlienController {
     
     public AlienModel getModel() {
     	return model;
+    }
+    
+    public String toString(LinkedList<String> lista) {
+    	String result="";
+    	for(String s:lista)
+    		result+=s+" - ";
+    	return result.substring(0, result.length()-3)+"\n";
     }
 }
--- Lab02_Alien/src/it/polito/tdp/alien/model/AlienModel.java
+++ Lab02_Alien/src/it/polito/tdp/alien/model/AlienModel.java
@@ -3,21 +3,36 @@ package it.polito.tdp.alien.model;
 import java.util.*;
 
 public class AlienModel {
-	TreeMap<String, String> dizionario;
+	TreeMap<String, LinkedList<String>> dizionario;
 	
 	public AlienModel() {
-		dizionario = new TreeMap<String, String>();
+		dizionario = new TreeMap<String, LinkedList<String>>();
 	}
 	
+	/**
+	 * Add a word to the dictionary alien-italian if it is not present yet, or add a translation
+	 * @param w1 Alien word
+	 * @param w2 Italian word
+	 * @throws Exception Format incorrect (only letters accepted)
+	 */
 	public void addWord(String w1, String w2) throws Exception {
+		w1 = w1.toLowerCase();
+		w2 = w2.toLowerCase();
 		if(!isCorrect(w1) || !isCorrect(w2))
 			throw new Exception("Formato parola/e non corretto");
-		if(dizionario.containsKey(w1))
-			throw new Exception("Parola già inserita");
-		dizionario.put(w1, w2);
+		if(!dizionario.containsKey(w1))
+			dizionario.put(w1, new LinkedList<String>());
+		dizionario.get(w1).add(w2);
 	}
 	
-	public String findWord(String w) throws Exception {
+	/**
+	 * Search in the dictionary to find the translations of the given word
+	 * @param w The word in Alien
+	 * @return translations if present as LinkedList<String>
+	 * @throws Exception Incorrect format or word not present in the dictionary
+	 */
+	public LinkedList<String> findWord(String w) throws Exception {
+		w = w.toLowerCase();
 		if(!isCorrect(w))
 			throw new Exception("Formato parola/e non corretto");
 		if(!dizionario.containsKey(w))
